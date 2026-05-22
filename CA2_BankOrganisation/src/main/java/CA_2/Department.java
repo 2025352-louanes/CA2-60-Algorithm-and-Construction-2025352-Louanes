a31ecf0ad568dd2b34d00f739f8daffb0d422450
Lounes25CCT <lounes.ismal@gmail.com>
2026-05-22 14:58:16 +0100

    Created Department Java class with full documentation

---
+++ CA2_BankOrganisation/src/main/java/CA_2/Department.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author HP
+ */
+public class Department {
+     private String departmentName;
+     
+     public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+      public String getDepartmentName() {
+        return departmentName;
+    }
+       @Override
+    public String toString() {
+        return departmentName;
+    }
+}
