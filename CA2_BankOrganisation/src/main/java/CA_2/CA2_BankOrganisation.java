4e33ae03f36ef045e2cdd24c8b2c69bb4cdddca4
Lounes25CCT <lounes.ismal@gmail.com>
2026-05-22 13:00:34 +0100

    initialise Maven Java project for Bank organisation Project name: CA2_BankOrganisation

---
+++ CA2_BankOrganisation/src/main/java/CA_2/CA2_BankOrganisation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package CA_2;
+
+/**
+ *
+ * @author HP
+ */
+public class CA2_BankOrganisation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
