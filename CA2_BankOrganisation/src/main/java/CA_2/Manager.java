013c26e3549b76c2160203dde75883fbb3a702fc
Lounes25CCT <lounes.ismal@gmail.com>
2026-05-22 19:13:17 +0100

    Created Manager Java Class. Represents a Manager role within the organisation.

---
+++ CA2_BankOrganisation/src/main/java/CA_2/Manager.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author HP
+ */
+public class Manager {
+    private String managerType;
+
+    public Manager(String managerType) {
+        this.managerType = managerType;
+    }
+
+    public String getManagerType() {
+        return managerType;
+    }
+
+     @Override
+    public String toString() {
+        return managerType;
+    }
+
+}
