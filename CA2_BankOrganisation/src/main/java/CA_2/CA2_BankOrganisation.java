050ee261046254616744d0024061144f8a2963fd
Lounes25CCT <lounes.ismal@gmail.com>
2026-05-23 23:49:35 +0100

    Created Main class and improved Enum Menu system with dynamic menu display and employee listing

--- CA2_BankOrganisation/src/main/java/CA_2/CA2_BankOrganisation.java
+++ CA2_BankOrganisation/src/main/java/CA_2/CA2_BankOrganisation.java
@@ -3,14 +3,74 @@
  */
 
 package CA_2;
-
+import java.util.Scanner;
 /**
  *
  * @author HP
  */
 public class CA2_BankOrganisation {
-
+    
+    // Scanner object for user input
+    static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // Create FileHandler object
+        FileHandler fileHandler = new FileHandler();
+
+        // Read employees file
+        fileHandler.readFile("src/main/resources/Applicants_Form.txt");
+        
+        int choice;
+        
+        do{
+            // Display ENUM menu
+            MenuOption.displayMenu();
+
+            System.out.print("Select an option: ");
+            
+            // Input validation
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next();
+            }
+
+            choice = scanner.nextInt();
+            
+            switch (choice) {
+
+                case 1:
+                    System.out.println("SORT selected"); //Call sorting method
+                    break;
+
+                case 2:
+                    System.out.println("SEARCH selected"); //Call searching method
+                    break;
+                    
+                case 3:
+                    System.out.println("ADD RECORD selected"); 
+                    break;
+
+                case 4:
+                    System.out.println("CREATE BINARY TREE selected");
+                    break;
+
+                case 5:
+                    System.out.println("DISPLAY EMPLOYEES selected");
+                    
+                    //Display employees from file
+                    fileHandler.displayEmployees();
+                    break;
+                    
+                case 6:
+                    System.out.println("Exit program ");
+                    break;
+                    
+                default:
+                    System.out.println("Invalid option! Try again.");            
+           }
+            System.out.println();
+        
+        }while (choice != MenuOption.EXIT.getValue());
     }
 }
